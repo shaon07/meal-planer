72d342f73aeece2b5cd652b78bc10826d861a0c6
shaon07 <alishaon078@gmail.com>
2025-11-28 11:46:23 +0600

    refactor: useQueryParams hook for query param handle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,19 @@
-export const updateQueryParams = (tabValue: string) => {
+export const updateQueryParams = (key: string, value: string) => {
   const url = new URL(window.location.href);
-  url.searchParams.set("tab", tabValue);
+  url.searchParams.set(key, value);
   window.history.pushState({}, "", url.toString());
+};
+
+export const getQueryParams = () => {
+  const map: Record<string, string> = {};
+
+  if (typeof window === "undefined") return map;
+
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.forEach((value, key) => {
+    map[key] = value;
+  });
+
+  return map;
 };
