cb177a9221fb3f4acec7aaf7e519724b1510091a
shaon07 <alishaon078@gmail.com>
2025-11-28 11:59:37 +0600

    feat: added global error boundary component

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.tsx";
 import RootLayout from "./components/Layout/RootLayout.tsx";
+import { ErrorBoundary } from "./components/organisms/ErrorBoundary/index.tsx";
 import { store } from "./context/redux/store.ts";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RootLayout>
-        <App />
-      </RootLayout>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <RootLayout>
+          <App />
+        </RootLayout>
+      </Provider>
+    </ErrorBoundary>
   </StrictMode>
 );
